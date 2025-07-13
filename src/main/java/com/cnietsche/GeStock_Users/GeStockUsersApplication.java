9b43e5d884e5b609620a59b9166ac21ef2fed58d
Christian Nietsche <christiannietsche@hotmail.com>
2025-07-13 18:35:13 -0300

    First commit

---
+++ src/main/java/com/cnietsche/GeStock_Users/GeStockUsersApplication.java
@@ -0,0 +1,13 @@
+package com.cnietsche.GeStock_Users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeStockUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeStockUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cnietsche/GeStock_Users/GeStockUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cnietsche.GeStock_Users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeStockUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
